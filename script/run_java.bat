9555b0efb0afe6f36cfde051fa3bdc3f62aea920
wwing.you <winwing.you@gmail.com>
2024-02-29 11:05:22 +0900

    win exe encoding change

--- script/run_java.bat
+++ script/run_java.bat
@@ -1,25 +1,6 @@
 @echo off
 
-rem java 설치여부 확인
-where java > nul
-if errorlevel 1 (
-	echo 오류: "java"가 설치되어 있지 않습니다.
-	goto :EOF
-)
-rem javac 설치여부 확인
-where javac > nul
-if errorlevel 1 (
-	echo 오류: "javac"가 설치되어 있지 않습니다.
-	goto :EOF
-)
-
-rem java 파일 컴파일
 javac Solution.java
-
-if errorlevel 1 (
-	echo 오류: 컴파일 에러
-	goto :EOF
-)
 
 java -cp .;../../java;../../lib/jansi-2.4.0.jar nossi.Main < testcase.txt
 
